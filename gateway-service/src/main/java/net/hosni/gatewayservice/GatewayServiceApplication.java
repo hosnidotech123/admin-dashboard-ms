072dbb9409440959dd34d19077dd93984b0cd122
hosnidotech123 <hosnidotech@gmail.com>
2024-05-02 11:18:57 +0100

    services-n

--- gateway-service/src/main/java/net/hosni/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/hosni/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,15 @@ package net.hosni.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+
+
+
+
 
 
 @SpringBootApplication
@@ -9,6 +18,11 @@ public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
     }
 
 }
